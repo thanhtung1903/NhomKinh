87afcd28a8ad55d7e20371b0929311845d327b3f
huynh.thanh.tung <FRAMGIA\huynh.thanh.tung@i120492-pc.framgia.com>
2018-11-14 09:55:01 +0700

    init-project

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Admin::SessionsController < Devise::SessionsController
+  layout "admin_layout"
+
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  def destroy
+    super
+  end
+
+  private
+  def after_sign_in_path_for _admin
+    admin_path
+  end
+
+  def after_sign_out_path_for _admin
+    new_admin_sessions_path
+  end
+end
